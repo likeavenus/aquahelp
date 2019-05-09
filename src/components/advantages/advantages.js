78e5da6aa9f5e1ce7b9d92a82f9c746604cb7d2a
Rafael <rafael_guliev94@list.ru>
2019-05-09 16:45:37 +0300

    fix js

--- src/components/advantages/advantages.js
+++ src/components/advantages/advantages.js
@@ -4,7 +4,14 @@ export default function advantages() {
     slider.owlCarousel({
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                nav: false,
+                dots: false,
+                stagePadding: 25
+            },
+            768: {
+                items: 2,
+                stagePadding: 20
             }
         }
     });
